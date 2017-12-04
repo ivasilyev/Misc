b5965b8490796bb1938a90a6d3ad20d82e6892ec
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2017-12-04 18:52:30 +0300

    Updated regex_slicer

--- regex_slicer.py
+++ regex_slicer.py
@@ -1,43 +1,63 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# !/usr/bin/python
 
 import getopt
 import sys
 import re
-import numpy
 
 
 def usage():
     print("\nUsage: " + sys.argv[0] + " -i <file> -n <int> -p <regex>" + "\n\n" +
-          "-i/--input <file> \tA text  file" + "\n" +
-          "-n/--number <int> \tNumber of chunks to chop" + "\n" +
-          "-p/--pattern <regex> \tA pattern or regular expression to split" + "\n")
+          "-i/--input <file> \tA text  file;" + "\n" +
+          "-n/--number <int> \tNumber of chunks to chop;" + "\n" +
+          "-p/--pattern <regex> \tA pattern or regular expression to split supplied by backslash symbol if required;" + "\n" +
+          "-o/--output <regex> \tOutput directory." + "\n")
     sys.exit(2)
 
 
 def main():
     opts = ""
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:n:p:", ["help", "input=", "number=", "pattern="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:n:p:o:", ["help", "input=", "number=", "pattern=", "output="])
     except getopt.GetoptError as arg_err:
         print(str(arg_err))
         usage()
-    i = None
-    n = None
-    p = None
+    i, n, p = (None,) * 3
+    o = ''
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             usage()
         elif opt in ("-i", "--input"):
             i = str(arg)
         elif opt in ("-n", "--number"):
-            n = str(arg)
+            try:
+                n = int(arg)
+            except ValueError:
+                print("Chunks number must be integer! Exiting...")
+                sys.exit(2)
         elif opt in ("-p", "--pattern"):
             p = str(arg)
+        elif opt in ("-o", "--output"):
+            o = str(arg)
     if not any(var is None for var in [i, n]):
-        return i, n, p
+        return i, n, p, o
     print("The parameters are not yet specified!")
     usage()
+
+
+def is_path_exists(path):
+    import os
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        print(exception)
+
+
+def ends_with_slash(string):
+    if string.endswith("/"):
+        return string
+    else:
+        return str(string + "/")
 
 
 def file_to_str(file):
@@ -45,45 +65,59 @@ def file_to_str(file):
     return file_parsed
 
 
-def coding_python_version_check():
-    if sys.version_info >= (3, 3):
-        return "unicode-escape"  # for python 3.3+ try "unicode-escape"
-    else:
-        return "string-escape"
-
-
-def chop(string, number_of_chunks, pattern):
-    number_of_chunks = int(number_of_chunks)
+def regex_split(string, pattern):
     if pattern is None:
         pattern = "\n"
-    max_chunks = len(re.findall(pattern, string))
+    print("Using pattern: " + repr(pattern))
+    list_to_join = list(filter(None, re.split("(" + pattern + ")", string)))
+    return list_to_join
+
+
+def filename_only(string):
+    return str(".".join(string.rsplit("/", 1)[-1].split(".")[:-1]))
+
+
+def list_chop(input_list, number_of_chunks):
+    max_chunks = len(input_list)
     if max_chunks < number_of_chunks:
         print("Cannot make " + str(number_of_chunks) + " chunks! \n" +
               "Will make " + str(max_chunks) + " chunks.")
         number_of_chunks = max_chunks
-    list_to_join = list(filter(None, re.split("(" + str(pattern).decode(coding_python_version_check()) + ")", string)))
-    output = []
-    for numpy_array in numpy.array_split(numpy.array([i + j for i, j in zip(list_to_join[::2], list_to_join[1::2])]), int(number_of_chunks)):
-        output.append(numpy_array.tolist())
-    return output
+    chunks_length = int(len(input_list) / number_of_chunks)
+    if sys.version_info >= (3, 0):
+        chunks_list = [input_list[i:i + chunks_length] for i in range(0, (number_of_chunks - 1) * chunks_length, chunks_length)]
+    else:
+        chunks_list = [input_list[i:i + chunks_length] for i in xrange(0, (number_of_chunks - 1) * chunks_length, chunks_length)]
+    chunks_list.append(input_list[(number_of_chunks - 1) * int(chunks_length):])
+    return chunks_list
 
 
 def export_two_dimensional_array(array):
     chunk = 1
+    chunks_list = []
     for dim in array:
-        list_to_file("", dim, str('.'.join(inputFile.split('.')[:-1]) + '_chunk_' + str(chunk) + '.' + inputFile.split('.')[-1]))
+        output_file_name = outputDir + filename_only(inputFile) + "_chunk_" + str(chunk) + '.' + inputFile.split('.')[-1]
+        list_to_file('', dim, output_file_name)
+        chunks_list.append(output_file_name + '\n')
         chunk += 1
+    chunks_list_file_name = outputDir + filename_only(inputFile) + ".chunkslist"
+    list_to_file('', chunks_list, chunks_list_file_name)
+    print("The chunks list saved into", chunks_list_file_name)
 
 
 def list_to_file(header, list_to_write, file_to_write):
-    header += "".join(str(i) for i in list_to_write if i if i is not None)
+    header += ''.join(str(i) for i in list_to_write if i is not None)
     file = open(file_to_write, 'w')
     file.write(header)
     file.close()
 
 
 ########################################################
-inputFile, chunksNumber, inputPattern = main()
+inputFile, chunksNumber, inputPattern, outputDir = main()
+is_path_exists(outputDir)
+outputDir = ends_with_slash(outputDir)
 rawStrings = file_to_str(inputFile)
-processedArray = chop(rawStrings, chunksNumber, inputPattern)
+rawList = regex_split(rawStrings, inputPattern)
+processedArray = list_chop(rawList, chunksNumber)
 export_two_dimensional_array(processedArray)
+print("File slicing completed")

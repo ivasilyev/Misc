cdedab024fc2d2a987ab10b5b95f1f2f0e312559
ivasilyev <u0412u0418u042e@gmail.com>
2024-08-30 21:00:03 +0300

    Removed redundant script

--- windows/add_defender_exclusion.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM Run this script with elevated permissions
-
-echo Add Windows Defender exclusion
-powershell -inputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath '%PROGRAM_PATH%'
-timeout 15

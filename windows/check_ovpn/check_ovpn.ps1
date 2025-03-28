c8a4f508d32cf31d80b9837b3bf6e0730cd4fe5b
ivasilyev <u0412u0418u042e@gmail.com>
2025-03-29 00:46:42 +0300

    Added logging & background run into the script to check OpenVPN connection

--- windows/check_ovpn/check_ovpn.ps1
+++ windows/check_ovpn/check_ovpn.ps1
@@ -1,6 +1,10 @@
 # Define variables
-$vpnProfile = "MyCon.ovpn"
-$openVpnGuiPath = "$Env:programfiles\OpenVPN\bin\openvpn-gui.exe"
+# Not using "$Env:USERPROFILE" here
+$userName = "User"
+$vpnProfile = "MyCon"
+$vpnProfileFile = "C:\Users\$userName\OpenVPN\config\$vpnProfile.ovpn"
+$vpnLogFile = "C:\Users\$userName\OpenVPN\log\$vpnProfile.log"
+$openVpnDirectoryPath = "$Env:PROGRAMFILES\OpenVPN\bin"
 $vpnConnectionName = "OpenVPN TAP-Windows6"
 $vpnConnectionLine = netsh int ipv4 show interfaces | findstr /C:"$vpnConnectionName"
 $vpnConnectionStatus = $vpnConnectionLine -Match " connected "
@@ -8,13 +12,14 @@ $vpnConnectionStatus = $vpnConnectionLine -Match " connected "
 if ($vpnConnectionStatus) {
     Write-Host "VPN connection is active." -ForegroundColor Green
 } else {
-    Write-Host "VPN connection is DOWN! Restarting OpenVPN..." -ForegroundColor Red
+    Write-Host "VPN connection is down! Restarting OpenVPN..." -ForegroundColor Red
 
     Write-Host "Kill all OpenVPN processes using taskkill"
     cmd /c "taskkill /F /IM openvpn.exe /IM openvpn-gui.exe"
 
     Write-Host "Start OpenVPN GUI with the connection profile"
-    Start-Process -FilePath $openVpnGuiPath -ArgumentList "--connect `"$vpnProfile`"" -NoNewWindow
+    # Start-Process -FilePath $openVpnDirectoryPath\openvpn-gui.exe -ArgumentList "--connect `"$vpnProfileFile`"" -NoNewWindow
+    Start-Process -FilePath $openVpnDirectoryPath\openvpn.exe -ArgumentList "--config `"$vpnProfileFile`" --log `"$vpnLogFile`"" -NoNewWindow
 
     Write-Host "OpenVPN restarted with profile: $vpnProfile" -ForegroundColor Yellow
 }

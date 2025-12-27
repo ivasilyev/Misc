c11952017bae22ddc1373ded7db06fee70f3ba4a
ivasilyev <u0412u0418u042e@gmail.com>
2025-12-27 23:44:57 +0300

    Added command to show OpenVPN network connection into the setup script

--- windows/check_all_disks/check_all_disks.bat
+++ windows/check_all_disks/check_all_disks.bat
@@ -1,22 +1,8 @@
 @echo off
-
-setlocal ENABLEDELAYEDEXPANSION
-
-set "CWD=%cd%"
-
+chcp 65001 > nul
 cd /d "%~dp0"
 
-mkdir "logs"
-
-for /f "skip=1 delims=" %%i in ('"%SystemRoot%\System32\wbem\wmic.exe" logicaldisk get caption') do (
-    set j=%%i
-    set j=!j::=!
-    echo Process !j!
-    timeout 1
-    
-    start "" check_disk.bat !j!
-)
-
-cd /d "%CWD%"
+title Backup
+powershell -NoLogo -ExecutionPolicy Bypass -Command "%cd%\%~n0.ps1"
 
 pause

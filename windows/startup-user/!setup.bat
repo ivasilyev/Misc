f90c6f7800fd7c7c681f9e80a839cf1d629ad75a
ivasilyev <u0412u0418u042e@gmail.com>
2025-12-28 01:26:51 +0300

    Refactoring for the user related startup scripts

---
+++ windows/startup-user/!setup.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set "SCRIPT_DIR=%~dp0"
+set "BASE_NAME=StartupUser"
+set "TARGET_DIR=C:\!SCRIPTS\%BASE_NAME%\"
+
+md "%TARGET_DIR%"
+
+echo Create environment for admin scripts
+
+cd /d "%SCRIPT_DIR%"
+
+REM Add scripts to copy
+for %%f in (
+    %BASE_NAME%.bat
+) do copy "%%f" "%TARGET_DIR%"
+
+
+echo Start scheduler to import the profile
+powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -Command "$TaskName = '%BASE_NAME%' ;$TaskXml = [xml](Get-Content '%BASE_NAME%.xml'); Register-ScheduledTask -TaskName $TaskName -Xml $TaskXml.OuterXml; Get-ScheduledTask -TaskName $TaskName"
+
+echo Then go to Security options and use the Administrators group
+start "" taskschd.msc
+
+start "" explorer "%TARGET_DIR%"
+
+timeout 5
---
+++ windows/startup-user/StartupUser.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set "SCRIPT_DIR=%~dp0"
+
+cd /d "%SCRIPT_DIR%"
+
+echo Minimize all windows and show the Desktop
+cd /d "%SCRIPT_DIR%"
+powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -Command "(New-Object -ComObject shell.application).toggleDesktop()"
+timeout 1
+
+echo Lock PC
+rundll32.exe user32.dll,LockWorkStation
+
+REM pause

a8934620dc2901b4b5ffba108c1fe5752b4c13e1
ivasilyev <u0412u0418u042e@gmail.com>
2025-12-27 23:44:43 +0300

    Added command to show OpenVPN network connection into the setup script

--- windows/check_all_disks/check_all_disks.ps1
+++ windows/check_all_disks/check_all_disks.ps1
@@ -1,22 +1,28 @@
 
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-cd $scriptPath
+Set-Location -Path "${PSScriptRoot}"
 
 mkdir "logs" -Force
 
 wmic logicaldisk get name /value 2>&1 | ForEach-Object {
     $string = $_.Trim()
-    if ( $string.Length -gt 0 ) {
+    if ( ${string}.Length -gt 0 ) {
         $letter = $string.Split("=")[-1]
-        echo "$letter"
-        Start-Process powershell "
-            `$Host.UI.RawUI.WindowTitle=`'Check disk $letter`'
-            echo 'Check disk $letter'
-            chkntfs /c $($letter.ToLower())
-            & echo Y | chkdsk /x /f /r $letter 2>&1 | Tee-Object logs\chkdsk-$($letter.Trim(':')).log
-            timeout 5
-        "
+        if ( ${letter}.equals("C:") ) {
+            # Do not check the system disk
+            Write-Host -ForegroundColor Gray "Skip ${letter}";
+        } else {
+            Start-Process powershell "
+                `$logFile = 'logs\chkdsk-$($letter.Trim(':')).log'
+                `$msg = 'Check disk ${letter}';
+                `${Host}.UI.RawUI.WindowTitle=`${msg};
+                Write-Host -ForegroundColor Cyan `"`${msg}`";
+                chkntfs /c ${letter}.ToLower() 2>&1 | Tee-Object '${logFile}';
+                & echo Y | chkdsk /x /f /r $letter 2>&1 | Tee-Object '${logFile}';
+                Start-Sleep -Seconds 5;
+            "
+        }
     }
 }
 
-pause
+Write-Host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')

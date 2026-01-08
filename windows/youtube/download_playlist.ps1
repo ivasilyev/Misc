b218b48380de8b9025f63f342e0470f0e43f8e35
ivasilyev <u0412u0418u042e@gmail.com>
2026-01-08 23:49:28 +0300

    Youtube video download invocation moved to a separate script

--- windows/youtube/download_playlist.ps1
+++ windows/youtube/download_playlist.ps1
@@ -9,15 +9,15 @@ param(
 # Usage: 
 # powershell -NoLogo -ExecutionPolicy Bypass -Command "download_playlist.ps1 -playlistUrl 'https://www.youtube.com/@playlist/videos' -downloadFolder 'C:\playlist'"
 
-Write-Host "Params: playlistUrl=$playlistUrl downloadFolder=$downloadFolder"
+$scriptPath = split-path -parent ${MyInvocation}.MyCommand.Definition
+Set-Location -Path "${scriptPath}"
 
-$yt = "yt-dlp.exe"
+Write-Host "Params: playlistUrl='$playlistUrl' downloadFolder='$downloadFolder'"
 
 $timestamp = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
 $tempFile = Join-Path -Path $env:TEMP -ChildPath ("yt_ids_{0}.txt" -f "${timestamp}")
 
-Set-Location `
-	-Path "${PSScriptRoot}"
+$yt = Join-Path "${PSScriptRoot}" "yt-dlp.exe"
 &"${yt}" `
 	'--ignore-errors' `
 	'--flat-playlist' `
@@ -29,7 +29,7 @@ Write-Host "Saved video IDs to temp file: '${tempFile}'"
 
 $ids = Get-Content -Path "${tempFile}" | Where-Object { $_.Trim().Length -gt 0 }
 New-Item -ItemType Directory -Force -Path "${downloadFolder}"
-
+$downloader = Join-Path "${PSScriptRoot}" "download_video.ps1"
 foreach ($id in $ids) {
     $exists = Get-ChildItem `
 		-Path "${downloadFolder}" `
@@ -40,36 +40,7 @@ foreach ($id in $ids) {
     if (!$exists) {
         Write-Host -ForegroundColor Green "Download ID '${id}'"
         $videoUrl = "https://www.youtube.com/watch?v=${id}"
-		&"${yt}" `
-			'--abort-on-unavailable-fragment' `
-			'--mtime' `
-			'--embed-chapters' `
-			'--embed-metadata' `
-			'--min-sleep-interval=60' `
-			'--max-sleep-interval=90' `
-			'--sleep-interval=15' `
-			'--sleep-requests=3' `
-			'--sleep-subtitles=3' `
-			'--proxy=socks5://127.0.0.1:8080' `
-			'--cookies-from-browser=firefox:12345678.default-release' `
-			'--no-skip-unavailable-fragments' `
-			'--no-abort-on-unavailable-fragments' `
-			'--no-skip-unavailable-fragments' `
-			'--no-check-certificates' `
-			'--retries=100' `
-			'--fragment-retries=100' `
-			'--add-metadata' `
-			'--all-subs' `
-			'--convert-subs=ass' `
-			'--embed-subs' `
-			'--format=bestvideo+bestaudio/best' `
-			'--preset-alias=mkv' `
-			'--merge-output-format=mkv' `
-			'--remux-video=mkv' `
-			'--output' `
-			"${downloadFolder}\%(title)s__%(id)s.%(ext)s" `
-			'--verbose' `
-			"${videoUrl}"
+		&"${downloader}" -videoUrl "${videoUrl}" -downloadFolder "${downloadFolder}"
     } else {
         Write-Host -ForegroundColor Cyan "Skip ID '${id}'"
     }

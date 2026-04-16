fab37d8f45f3488601d9903ca77ba78fd9e2eadb
ivasilyev <u0412u0418u042e@gmail.com>
2026-04-16 17:17:28 +0300

    Added proxy support into video playlist download script

--- windows/youtube/download_playlist.ps1
+++ windows/youtube/download_playlist.ps1
@@ -1,12 +1,12 @@
 param(
-  [Parameter(Mandatory=$true)]
-  [string] $playlistUrl,
+    [Parameter(Mandatory=$true)]
+    [string] $playlistUrl,
 
-  [Parameter(Mandatory=$true)]
-  [string] $downloadFolder,
+    [Parameter(Mandatory=$true)]
+    [string] $downloadFolder,
 
-  [Parameter(Mandatory=$true)]
-  [string] $videoUrlPrefix
+    [Parameter(Mandatory=$true)]
+    [string] $videoUrlPrefix
 )
 
 # Usage: 
@@ -14,45 +14,45 @@ param(
 
 $cookiesBrowser = "firefox:sessionId.default-release"
 
-$scriptPath = split-path -parent ${MyInvocation}.MyCommand.Definition
-Set-Location -Path "${scriptPath}"
-
 $videoUrlPrefix=${videoUrlPrefix}.Trim()
-
-Write-Host "Params: playlistUrl='${playlistUrl}' downloadFolder='${downloadFolder}' videoUrlPrefix='${videoUrlPrefix}'"
-
 $timestamp = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
 $tempFile = Join-Path -Path $env:TEMP -ChildPath ("yt_ids_{0}.txt" -f "${timestamp}")
-
 $yt = Join-Path "${PSScriptRoot}" "yt-dlp.exe"
-&"${yt}" `
-  "--cookies-from-browser=${cookiesBrowser}" `
-  "--ignore-errors" `
-  "--flat-playlist" `
-  "--print-to-file=%(id)s" `
-  "${tempFile}" `
-  "${playlistUrl}"
 
-Write-Host "Saved video IDs to temp file: '${tempFile}'"
+Write-Host -ForegroundColor Cyan "Params: playlistUrl='${playlistUrl}' downloadFolder='${downloadFolder}' videoUrlPrefix='${videoUrlPrefix}'"
+
+Write-Host -ForegroundColor Cyan "Get video IDs to temp file: '${tempFile}'"
+Write-Host -ForegroundColor Cyan "The current time is: $(Get-Date -UFormat "%Y.%m.%d %H:%M:%S")"
+Set-Location -Path "${PSScriptRoot}"
+&"${yt}" `
+    "--cookies-from-browser=${cookiesBrowser}" `
+    "--ignore-errors" `
+    "--flat-playlist" `
+    "--print-to-file=%(id)s" `
+    "${tempFile}" `
+    "${playlistUrl}"
+#     "--proxy=socks5://127.0.0.1:1080" `
+
+Write-Host -ForegroundColor Cyan "Saved video IDs to temp file: '${tempFile}'"
 
 $ids = Get-Content -Path "${tempFile}" | Where-Object { $_.Trim().Length -gt 0 }
 New-Item -ItemType Directory -Force -Path "${downloadFolder}"
 $downloader = Join-Path "${PSScriptRoot}" "download_video.ps1"
 foreach ($id in $ids) {
-  $exists = Get-ChildItem `
-  -Path "${downloadFolder}" `
-  -Recurse `
-  -Filter "*__${id}.mkv" `
-  -File `
-  -ErrorAction SilentlyContinue
-  if (!$exists) {
-    Write-Host -ForegroundColor Green "Download ID '${id}'"
-    $videoUrl = "${videoUrlPrefix}${id}"
-  &"${downloader}" -videoUrl "${videoUrl}" -downloadFolder "${downloadFolder}"
-  } else {
-    Write-Host -ForegroundColor Cyan "Skip ID '${id}'"
-  }
+    $exists = Get-ChildItem `
+        -Path "${downloadFolder}" `
+        -Recurse `
+        -Filter "*__${id}.mkv" `
+        -File `
+        -ErrorAction SilentlyContinue
+    if (!$exists) {
+        Write-Host -ForegroundColor Green "Download ID '${id}'"
+        $videoUrl = "${videoUrlPrefix}${id}"
+        &"${downloader}" -videoUrl "${videoUrl}" -downloadFolder "${downloadFolder}"
+    } else {
+        Write-Host -ForegroundColor Yellow "Skip ID '${id}'"
+    }
 }
 
-Write-Host "Remove temp file '${tempFile}'"
+Write-Host -ForegroundColor Cyan "Remove temp file '${tempFile}'"
 Remove-Item -Path "${tempFile}" -ErrorAction SilentlyContinue

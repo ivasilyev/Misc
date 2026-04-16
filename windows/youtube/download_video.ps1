2a97cfce8aa2affac5630130c7532958287cb525
ivasilyev <u0412u0418u042e@gmail.com>
2026-04-16 17:13:05 +0300

    Added cleanup into video download script

--- windows/youtube/download_video.ps1
+++ windows/youtube/download_video.ps1
@@ -14,39 +14,54 @@ $cookiesBrowser = "firefox:sessionId.default-release"
 $scriptPath = split-path -parent ${MyInvocation}.MyCommand.Definition
 Set-Location -Path "${scriptPath}"
 
-Write-Host "Params: videoUrl='$videoUrl' downloadFolder='$downloadFolder'"
+Write-Host -ForegroundColor Cyan "Params: videoUrl='$videoUrl' downloadFolder='$downloadFolder'"
 New-Item `
-  -Force `
-  -ItemType Directory `
-  -Path "${downloadFolder}"
+    -Force `
+    -ItemType Directory `
+    -Path "${downloadFolder}"
 
+Write-Host -ForegroundColor Cyan "The current time is: $(Get-Date -UFormat "%Y.%m.%d %H:%M:%S")"
 $yt = Join-Path "${PSScriptRoot}" "yt-dlp.exe"
 &"${yt}" `
-  "--abort-on-unavailable-fragment" `
-  "--mtime" `
-  "--embed-chapters" `
-  "--embed-metadata" `
-  "--min-sleep-interval=60" `
-  "--max-sleep-interval=90" `
-  "--sleep-interval=15" `
-  "--sleep-requests=3" `
-  "--sleep-subtitles=3" `
-  "--cookies-from-browser=${cookiesBrowser}" `
-  "--match-filter=!is_live" `
-  "--no-skip-unavailable-fragments" `
-  "--no-abort-on-unavailable-fragments" `
-  "--no-skip-unavailable-fragments" `
-  "--no-check-certificates" `
-  "--retries=100" `
-  "--fragment-retries=100" `
-  "--add-metadata" `
-  "--all-subs" `
-  "--convert-subs=ass" `
-  "--embed-subs" `
-  "--format=bestvideo+bestaudio/best" `
-  "--preset-alias=mkv" `
-  "--merge-output-format=mkv" `
-  "--remux-video=mkv" `
-  "--output=${downloadFolder}\%(title)s__%(id)s.%(ext)s" `
-  "--verbose" `
-  "${videoUrl}"
+    "--abort-on-unavailable-fragment" `
+    "--mtime" `
+    "--embed-chapters" `
+    "--embed-metadata" `
+    "--min-sleep-interval=60" `
+    "--max-sleep-interval=90" `
+    "--sleep-interval=15" `
+    "--sleep-requests=3" `
+    "--sleep-subtitles=3" `
+    "--cookies-from-browser=${cookiesBrowser}" `
+    "--match-filter=!is_live" `
+    "--no-skip-unavailable-fragments" `
+    "--no-abort-on-unavailable-fragments" `
+    "--no-skip-unavailable-fragments" `
+    "--no-check-certificates" `
+    "--retries=100" `
+    "--fragment-retries=100" `
+    "--add-metadata" `
+    "--all-subs" `
+    "--convert-subs=ass" `
+    "--embed-subs" `
+    "--format=bestvideo+bestaudio/best" `
+    "--preset-alias=mkv" `
+    "--merge-output-format=mkv" `
+    "--remux-video=mkv" `
+    "--output=${downloadFolder}\%(title)s__%(id)s.%(ext)s" `
+    "--verbose" `
+    "${videoUrl}"
+#     "--proxy=socks5://127.0.0.1:1080" `
+
+
+Write-Host -ForegroundColor Cyan "Cleanup"
+$extensionsToRemove = @("*.xml", "*.json", "*.ytdl", "*.part")
+Get-ChildItem -Path "${downloadFolder}" -Include $extensionsToRemove -Recurse -File | ForEach-Object {
+    try {
+        Remove-Item -LiteralPath $_.FullName -Force -ErrorAction Stop
+        Write-Host "Removed: $($_.FullName)" -ForegroundColor Green
+    }
+    catch {
+        Write-Warning "Cannot remove $($_.FullName): $($_.Exception.Message)"
+    }
+}

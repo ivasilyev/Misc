3534e72543fcfa7325584714a950081404428939
ivasilyev <u0412u0418u042e@gmail.com>
2026-04-16 17:07:55 +0300

    Refactored binaries update script

--- windows/youtube/update_youtube-dl.ps1
+++ windows/youtube/update_youtube-dl.ps1
@@ -1,50 +1,57 @@
 
 Set-Location -Path "${PSScriptRoot}"
 
-$repository = "ytdl-org/youtube-dl"
-Write-Host -ForegroundColor Cyan "Update repository ${repository}"
-$releases_url = "https://api.github.com/repos/${repository}/releases"
-Write-Host -ForegroundColor Cyan "Get latest release" 
-$release_tag = (Invoke-WebRequest "${releases_url}" | ConvertFrom-Json)[0].tag_name
-Write-Host -ForegroundColor Cyan "Dowload latest release: ${release_tag}"
-$file = "youtube-dl.exe"
-Stop-Process -Force -Name youtube-dl 2>"${NULL}"
-$wc = New-Object net.webclient
-$wc.Downloadfile("https://github.com/${repository}/releases/download/${release_tag}/${file}", "${file}")
+function downloadGitRelease {
+    param (
+        [string]$repository,
+        [string]$file
+    )
+    Write-Host -ForegroundColor Cyan "The current time is: $(Get-Date -UFormat "%Y.%m.%d %H:%M:%S")"
+    Write-Host -ForegroundColor Cyan "Update repository ${repository}"
+    $releases_url = "https://api.github.com/repos/${repository}/releases"
+    Write-Host -ForegroundColor Cyan "Get latest release"
+    $release_json = Invoke-WebRequest "${releases_url}"
+    $release_tag = ("${release_json}" | ConvertFrom-Json)[0].tag_name
+    Write-Host -ForegroundColor Cyan "Download latest release: ${release_tag}"
+    $wc = New-Object net.webclient
+    $wc.Downloadfile("https://github.com/${repository}/releases/download/${release_tag}/${file}", "${file}")
+}
 
-$repository = "yt-dlp/yt-dlp"
-Write-Host -ForegroundColor Cyan "Update repository ${repository}"
-$releases_url = "https://api.github.com/repos/${repository}/releases"
-Write-Host -ForegroundColor Cyan "Get latest release" 
-$release_tag = (Invoke-WebRequest "${releases_url}" | ConvertFrom-Json)[0].tag_name
-Write-Host -ForegroundColor Cyan "Dowload latest release: ${release_tag}"
-$file = "yt-dlp.exe"
-Stop-Process -Force -Name yt-dlp 2>"${NULL}"
-$wc.Downloadfile("https://github.com/${repository}/releases/download/${release_tag}/${file}", "${file}")
 
+# youtube-dl
+Stop-Process -Force -Name "youtube-dl" 2>"${NULL}"
+downloadGitRelease -repository "ytdl-org/youtube-dl" -file "youtube-dl.exe"
+
+# yt-dlp
+Stop-Process -Force -Name "yt-dlp" 2>"${NULL}"
+downloadGitRelease -repository "yt-dlp/yt-dlp" -file "yt-dlp.exe"
+
+# FFmpeg
 $repository = "BtbN/FFmpeg-Builds"
 Write-Host -ForegroundColor Cyan "Update repository ${repository}"
 $releases_url = "https://api.github.com/repos/${repository}/releases"
 Write-Host -ForegroundColor Cyan "Get latest release"
 $release_tag = (Invoke-WebRequest "${releases_url}" | ConvertFrom-Json)[0].tag_name
 Write-Host -ForegroundColor Cyan "Dowload latest release: ${release_tag}"
+Stop-Process -Force -Name "ffmpeg" 2>"${NULL}"
 $folder = "ffmpeg-master-${release_tag}-win64-gpl"
 $file = "${folder}.zip"
+$wc = New-Object net.webclient
 $wc.Downloadfile("https://github.com/${repository}/releases/download/${release_tag}/${file}", "${file}")
 Expand-Archive `
-  -DestinationPath . `
-  -Force `
-  "${file}"
+    -DestinationPath . `
+    -Force `
+    "${file}"
 foreach ($processName in @("ffmpeg", "ffplay", "ffprobe")) {
-    Stop-Process -Force -Name ${processName} 2>"${NULL}"
+  Stop-Process -Force -Name ${processName} 2>"${NULL}"
 }
 Get-ChildItem `
-  -File `
-  -Path "${folder}\bin" `
-  -Recurse `
+    -File `
+    -Path "${folder}\bin" `
+    -Recurse `
 | Move-Item `
-  -Destination . `
-  -Force
+    -Destination . `
+    -Force
 Write-Host -ForegroundColor Cyan "Cleanup"
 foreach ($path in @("${folder}", "${file}")) {
   Remove-Item `
@@ -53,20 +60,15 @@ foreach ($path in @("${folder}", "${file}")) {
     -Recurse
 }
 
-$repository = "denoland/deno"
-Write-Host -ForegroundColor Cyan "Update repository ${repository}"
-$releases_url = "https://api.github.com/repos/${repository}/releases"
-Write-Host -ForegroundColor Cyan "Get latest release"
-$release_tag = (Invoke-WebRequest "${releases_url}" | ConvertFrom-Json)[0].tag_name
-Write-Host -ForegroundColor Cyan "Dowload latest release: ${release_tag}"
+# deno
 $file = "deno-x86_64-pc-windows-msvc.zip"
-$wc.Downloadfile("https://github.com/${repository}/releases/download/${release_tag}/${file}", "${file}")
-Stop-Process -Force -Name deno 2>"${NULL}"
+Stop-Process -Force -Name "deno" 2>"${NULL}"
+downloadGitRelease -repository "denoland/deno" -file "${file}"
 Expand-Archive `
-  -DestinationPath . `
-  -Force `
-  "${file}"
+    -DestinationPath . `
+    -Force `
+    "${file}"
 Remove-Item `
-  -Force `
-  -LiteralPath "${file}" `
-  -Recurse
+    -Force `
+    -LiteralPath "${file}" `
+    -Recurse

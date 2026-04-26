05b7400e5d30cdbe8c2d0c39bed79a666b99e6fa
ivasilyev <u0412u0418u042e@gmail.com>
2026-04-26 19:16:02 +0300

    Updated yt-dlp download wrapper script

--- windows/youtube/download_video.bat
+++ windows/youtube/download_video.bat
@@ -1,9 +1,12 @@
 @echo off
+chcp 65001 > NUL
+cd /d "%~dp0"
+
 set BAR=========================================================================
 
 echo(
 echo %BAR%
-echo Enter video (or playlist) URL: 
+echo Enter the video (NOT playlist) URL:
 echo %BAR%
 echo(
 echo(
@@ -14,49 +17,17 @@ echo(
 echo(
 echo(
 
-rem Download youtube-dl: https://yt-dl.org/downloads/latest/youtube-dl.exe
-rem Download ffdshow: https://ffmpeg.zeranoe.com/builds/
-rem Put youtube-dl.exe and ffdshow.exe into the same folder!
-rem Windows executable binary files require Microsoft Visual C++ 2010 Redistributable Package (x86) 
-
 echo %BAR%
-echo Downloading video with highest quality.
+echo Download video with highest quality.
 echo %BAR%
 echo(
 echo(
 echo(
-
-cd /D "%~dp0"
-rem mkdir download
-rem youtube-dl.exe --verbose --all-subs -f bestvideo+bestaudio/best -o download\%%(title)s__%%(id)s.%%(ext)s "%URL%" 
-rem echo Done, check the folder: '%cd%\download'.
-rem set DL_DIR="%HOMEDRIVE%%HOMEPATH%\Videos\youtube-dl"
 
 set "DL_DIR=%USERPROFILE%\Videos\youtube-dl"
+mkdir "%DL_DIR%"
 
-
-mkdir %DL_DIR%
-
-yt-dlp.exe ^
-    --abort-on-unavailable-fragment ^
-    --embed-chapters ^
-    --embed-metadata ^
-    --no-skip-unavailable-fragments ^
-    --no-check-certificates ^
-    --retries 100 ^
-    --add-metadata ^
-    --all-subs ^
-    --convert-subs=ass ^
-    --embed-subs ^
-    --format bestvideo+bestaudio/best ^
-    --preset-alias mkv ^
-    --merge-output-format mkv ^
-    --remux-video mkv ^
-    --output "%DL_DIR%\%%(title)s__%%(id)s.%%(ext)s" ^
-    --verbose "%URL%"
-
-
-rem ^  --restrict-filenames
+powershell -NoLogo -ExecutionPolicy Bypass -File "%cd%\%~n0.ps1"
 
 echo(
 echo(

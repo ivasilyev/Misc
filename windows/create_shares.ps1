5b72c5e8f323a99d9487dfba6281fec9d564321a
ivasilyev <u0412u0418u042e@gmail.com>
2026-04-26 17:12:40 +0300

    Updated samba share powershell creation script

--- windows/create_shares.ps1
+++ windows/create_shares.ps1
@@ -1,7 +1,9 @@
 # Create the share folder
 
 # Get all disks with assigned drive letters
-$disks = Get-Volume | Where-Object { $_.DriveLetter -match "^[A-Z]$" }
+$disks = Get-Volume `
+| Where-Object { $_.DriveLetter -match "^[A-Z]$" } `
+| Sort-Object DriveLetter
 
 # Define folder name
 $folderName = "Share"
@@ -9,17 +11,23 @@ $folderName = "Share"
 # Define user to grant access
 $user = "user"
 
-foreach ($disk in $disks) {
+foreach ($disk in ${disks}) {
+    Write-Host "Process disk ${disk}" -ForegroundColor Gray
     # Define full folder path
     $letter = "$($disk.DriveLetter)"
-    $folderPath = "${letter}:\$folderName"
+    $folderPath = "${letter}:\${folderName}"
+
+    if (${letter} -eq $($($Env:SYSTEMDRIVE)[0])) {
+        Write-Host "Cancel sharing the one on the system drive" -ForegroundColor Yellow
+        continue
+    }
 
     # Create the folder if it doesn't exist
-    if (!(Test-Path -Path $folderPath)) {
-        New-Item -Path $folderPath -ItemType Directory | Out-Null
-        Write-Host "Created folder: $folderPath" -ForegroundColor Green
+    if (!(Test-Path -Path "${folderPath}")) {
+        New-Item -Path "${folderPath}" -ItemType Directory | Out-Null
+        Write-Host "Created folder: ${folderPath}" -ForegroundColor Green
     } else {
-        Write-Host "Folder already exists: $folderPath" -ForegroundColor Yellow
+        Write-Host "Folder already exists: ${folderPath}" -ForegroundColor Yellow
     }
 
     # Grant NTFS permissions to the user
@@ -34,22 +42,23 @@ foreach ($disk in $disks) {
 
     Write-Host "Granted NTFS permissions to '$user' on $folderPath" -ForegroundColor Green
 
-    # Share the folder
-    $shareName = $("${folderName}-${letter}").ToLower()
-    if (Get-SmbShare -Name $shareName -ErrorAction SilentlyContinue) {
-        Write-Host "Share $shareName already exists" -ForegroundColor Yellow
+    # Share the folder under the most compact name including the drive letter only
+    $shareName = $("${letter}").ToLower()
+    if (Get-SmbShare -Name "${shareName}" -ErrorAction SilentlyContinue) {
+        Write-Host "Share ${shareName} already exists" -ForegroundColor Yellow
         Remove-SmbShare -Name "${shareName}" -Force
     }
-    New-SmbShare -Name "${shareName}" -Description "Share on the disk ${letter}" -Path "${folderPath}" -FullAccess $user
-    Write-Host "Shared $folderPath as $shareName with full access for '$user'" -ForegroundColor Green
+    New-SmbShare `
+        -Name "${shareName}" `
+        -Description "Share on the disk ${letter}" `
+        -Path "${folderPath}" `
+        -FullAccess "${user}"
+    Write-Host "Shared ${folderPath} as ${shareName} with full access for '$user'" -ForegroundColor Green
 
     # Remove access for "Everyone" from the share permissions
-    Revoke-SmbShareAccess -Name $shareName -AccountName "Everyone" -Force
+    Revoke-SmbShareAccess -Name "${shareName}" -AccountName "Everyone" -Force
 
-    Write-Host "Removed 'Everyone' from share access of $shareName" -ForegroundColor Green
+    Write-Host "Removed 'Everyone' from share access of ${shareName}" -ForegroundColor Green
 }
-
-# Stop sharing the one on the system drive
-Remove-SmbShare -Name $("$folderName-$($($Env:SYSTEMDRIVE)[0])").ToLower() -Force
 
 Write-Host "All disks processed successfully!" -ForegroundColor Cyan
